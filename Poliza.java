b20a6cd843073a0a5a86983eeb2d87464041ae00
JariffA69 <2402064@utrivieramaya.edu.mx>
2026-02-20 21:27:27 -0500

    Se agregó el código realizado en netbeans de la clase base (Poliza)

--- Poliza.java
+++ Poliza.java
@@ -1,3 +1,23 @@
 public class Poliza {
-    
+
+    protected String nombreAsegurado;
+    protected String tipoPoliza;
+    protected String fechaInicio;
+    protected String fechaFin;
+
+    public Poliza(String nombreAsegurado, String tipoPoliza,
+            String fechaInicio, String fechaFin) {
+
+        this.nombreAsegurado = nombreAsegurado;
+        this.tipoPoliza = tipoPoliza;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public void mostrarVigencia() {
+        System.out.println("Nombre: " + nombreAsegurado);
+        System.out.println("Tipo de poliza: " + tipoPoliza);
+        System.out.println("Fecha inicio: " + fechaInicio);
+        System.out.println("Fecha fin: " + fechaFin);
+    }
 }
