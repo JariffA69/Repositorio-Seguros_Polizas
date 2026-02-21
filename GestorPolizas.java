dc9146ee5e509ed8a83f4aa9adf11139394d4420
JariffA69 <2402064@utrivieramaya.edu.mx>
2026-02-20 21:32:30 -0500

    Se agregó clase genérica para guardar pólizas (GestorPólizas)

--- GestorPolizas.java
+++ GestorPolizas.java
@@ -1,3 +1,26 @@
-public class GestorPolizas {
-    
+public class GestorPolizas<T extends Poliza> {
+
+    private T[] polizas;
+    private int contador = 0;
+
+    public GestorPolizas(T[] polizas) {
+        this.polizas = polizas;
+    }
+
+    public void agregarPoliza(T poliza) {
+        if (contador < polizas.length) {
+            polizas[contador] = poliza;
+            contador++;
+            System.out.println("Poliza agregada correctamente.");
+        } else {
+            System.out.println("No hay espacio disponible.");
+        }
+    }
+
+    public void mostrarPolizas() {
+        for (int i = 0; i < contador; i++) {
+            polizas[i].mostrarVigencia();
+            System.out.println("--------------------");
+        }
+    }
 }
